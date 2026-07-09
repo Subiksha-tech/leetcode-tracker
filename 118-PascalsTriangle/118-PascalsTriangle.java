64032cd1a4b16af957aa2f3245722448d831e5a7
Subiksha Suresh <subikshasuresh2006@gmail.com>
2026-07-09 10:07:28 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.java
@@ -0,0 +1,28 @@
+// Last updated: 09/07/2026, 10:07:28
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            // First and last elements are always 1
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(
+                        result.get(i - 1).get(j - 1) +
+                        result.get(i - 1).get(j)
+                    );
+                }
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+}
