63333c90bb54e15d3bce6ada7c57f709e73bcac2
Subiksha Suresh <subikshasuresh2006@gmail.com>
2026-07-28 15:56:48 +0530

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/version/python/131-PalindromePartitioning_20260728_155646.py
@@ -0,0 +1,27 @@
+# Last updated: 28/07/2026, 15:56:46
+1class Solution:
+2    def partition(self, s: str) -> List[List[str]]:
+3        result = []
+4        path = []
+5
+6        def isPalindrome(left, right):
+7            while left < right:
+8                if s[left] != s[right]:
+9                    return False
+10                left += 1
+11                right -= 1
+12            return True
+13
+14        def backtrack(start):
+15            if start == len(s):
+16                result.append(path[:])
+17                return
+18
+19            for end in range(start, len(s)):
+20                if isPalindrome(start, end):
+21                    path.append(s[start:end + 1])
+22                    backtrack(end + 1)
+23                    path.pop()
+24
+25        backtrack(0)
+26        return result
